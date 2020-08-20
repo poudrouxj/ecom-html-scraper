a6cda90e2dcfbc5cae1527a0c9b430706eb475ef
poudrouxj <jeanmichael.poudroux@gmail.com>
2020-08-20 15:30:37 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name="htmlscraper-pkg-jpoudroux",
+    version="0.0.1",
+    author="Jean-Michael Poudroux",
+    author_email="jeanmichael.poudroux@gmail.com",
+    description="A small web scraping application",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
