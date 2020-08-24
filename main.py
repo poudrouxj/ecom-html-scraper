8f3979526f912e61fe88e47c825303b668471b21
poudrouxj <jeanmichael.poudroux@gmail.com>
2020-08-24 19:27:22 +0200

    Added flag for request limiting

--- main.py
+++ main.py
@@ -16,9 +16,23 @@ if __name__ == "__main__":
         type=str,
         default='config.json',
         help='config file',
+        required=False
     )
 
+    cli_parser.add_argument(
+        '-t',
+        '--target_file',
+        dest='target_file',
+        type=str,
+        default='target.json',
+        help='Target specific URLs'
+        requred=True
+    )
+
+
+    # parse known args doesnt throw exception when adding additional args
     args, unknown = cli_parser.parse_known_args()
+
     parser = argparse.ArgumentParser(parents=[cli_parser], add_help=False)
 
     if args.config_file is not None:
@@ -26,6 +40,6 @@ if __name__ == "__main__":
             config = json.load(open(args.config_file))
             parser.set_defaults(**config)
 
-        args = parser.parse_args()
+    args = parser.parse_args()
 
     main(vars(args))
